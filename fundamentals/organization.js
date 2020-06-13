28b7bc8779c033cc02b8ec90e4e55942d9f0703a
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-06-13 10:58:16 -0300

    First commit, updating the current repository with the programs already made

---
+++ fundamentals/organization.js
@@ -0,0 +1,9 @@
+console.log(
+    "Sentença de código")
+
+{
+    {
+        console.log("Hello, ");
+        console.log('world!!')  //Padrão do curso, não usa ponto e vírgula e aspas simples
+    }
+}
