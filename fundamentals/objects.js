2609f7fdd31f24f7a5a55f3be586347f7e3cc603
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-06-17 19:01:26 -0300

    Look there is a new object!!

---
+++ fundamentals/objects.js
@@ -0,0 +1,14 @@
+const product1 = {}
+product1.name = 'Smart'
+product1.price = 4998.90
+
+console.log(product1)
+
+const product2 = {
+    name: 'Shirt',
+    price: 49.90,
+    newProduct: {
+        name: 'Jeans',
+        price: 79.90
+    }
+}
