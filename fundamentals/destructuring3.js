5ec573a254503eba0bca7d653671e6b866c6d31e
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-07-09 21:17:08 -0300

    Commit with functions using destructuring

---
+++ fundamentals/destructuring3.js
@@ -0,0 +1,12 @@
+function rand({ min = 0, max = 1000 }) {
+    const value = Math.random() * (max - min) + min
+    return Math.floor(value)
+}
+
+console.log(rand({ max: 50, min: 40 }))
+
+const obj = { max: 50, min: 40 }
+console.log(rand(obj))
+console.log(rand({ min: 955 }))
+console.log(rand({}))
+//console.log(rand())
