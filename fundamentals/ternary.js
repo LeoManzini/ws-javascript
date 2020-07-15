6faa5e19d8a858b5e16f8330edee52a6ad87d9d8
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-07-14 21:02:37 -0300

    Adding programs made for study tuesday

---
+++ fundamentals/ternary.js
@@ -0,0 +1,4 @@
+const result = grade => grade >= 6 ? 'Approved' : 'Disapproved'
+
+console.log(result(7.1))
+console.log(result(5.9))
---
+++ fundamentals/unaries.js
@@ -0,0 +1,10 @@
+let var1 = 1
+let var2 = 2
+
+var1++
+console.log(var1)
+--var1
+console.log(var1)
+
+console.log(++var1 === var2--)
+console.log(var1 === var2)
